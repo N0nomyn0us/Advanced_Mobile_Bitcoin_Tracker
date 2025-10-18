d01f8389b644e81d91de50821745ed7cb06c6e74
N0nomyn0us <147658627+N0nomyn0us@users.noreply.github.com>
2025-10-18 18:14:20 +0100

    Updated the SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ android {
         }
     }
     namespace = "com.example.advancedmobilebitcointracker"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.advancedmobilebitcointracker"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
